deca903e7901975c1199dd30ec4351f99b78e0db
Oriel Ramagole <o.ramagole@gmail.com>
2013-05-17 11:47:51 +0200

    Mvc4 Assignment

---
+++ Assignment/MvcMusicSore/ViewModels/StoreBrowseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcMusicSore.Models;
+
+namespace MvcMusicSore.ViewModels
+{
+  public class StoreBrowseViewModel
+  {
+    public Genre Genre { get; set; }
+
+    public List<Album> Albums { get; set; }
+  }
+}
---
+++ Assignment/MvcMusicSore/ViewModels/StoreIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MvcMusicSore.ViewModels
+{
+  public class StoreIndexViewModel
+  {
+    public int NumberOfGenres { get; set; }
+
+    public List<string> Genres { get; set; }
+  }
+}
